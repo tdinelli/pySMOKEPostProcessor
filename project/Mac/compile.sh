fee863397f9230b1b50253a3907158bc91351b46
Titodinelli <timoteo.dinelli@polimi.it>
2022-07-25 09:10:04 +0200

    Added project folder

---
+++ project/Mac/compile.sh
@@ -0,0 +1,23 @@
+
+SOURCE=""
+TARGETBIN=""
+OPENSMOKE="/Users/edoardo/Desktop/source"
+EIGEN="/usr/local/include/eigen3"
+BOOST_LIB="/usr/local/Cellar/boost/1.78.0/lib"
+BOOST_INC="/usr/local/Cellar/boost/1.78.0/include"
+
+g++ \
+	--std=c++17 \
+	-I $EIGEN \
+	-I $BOOST_INC \
+	-I $OPENSMOKE \
+	-L $BOOST_LIB \
+	-lboost_filesystem \
+	$SOURCE/main.cpp $SOURCE/CollectionOfClasses.cpp $SOURCE/PostProcessor.cpp \
+	-o $TARGETBIN/pyPostProcessor.o \
+	-O3 \
+	-Wall \
+	-DNDEBUG \
+	-fPIC \
+	-w \
+
