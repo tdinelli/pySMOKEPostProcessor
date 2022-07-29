ca990932dd7aa8edf73e2faff074ac5c0657c9b0
timoteo dinelli <timoteo.dinelli@polimi.it>
2022-07-29 16:18:43 +0200

    Added python file to extract reaction names from index

---
+++ KineticMechanism.py
@@ -0,0 +1,53 @@
+
+import xml.etree.ElementTree as ET
+import os
+
+class KineticMechanism:
+
+    def __init__(self, kineticmechanismFolder: str):
+    
+        reactionNames = os.path.join(kineticmechanismFolder, "reaction_names.xml")
+        kinetics = os.path.join(kineticmechanismFolder, "kinetics.xml")
+        
+        tree_kinetics = ET.parse(kinetics)
+        root_kinetics = tree_kinetics.getroot()
+
+        tree_names = ET.parse(reactionNames)
+        root_names = tree_names.getroot()
+
+        self.reaction_strings_ = root_names.find("reaction-names")
+        self.reaction_strings_ = (self.reaction_strings_.text).split()
+
+        kinetics = root_kinetics.find("Kinetics")
+        self.nr = int( (kinetics.find('NumberOfReactions')).text )
+        
+        FallOff = ((kinetics.find("FallOff")).text).split()
+        self.NumberOfFallOffReactions = int(FallOff[0])
+        listIndicesOfFallOffReactions = FallOff[1:]
+        self.IndicesOfFallOffReactions = [int(i) for i in listIndicesOfFallOffReactions]
+
+        Cabr = ((kinetics.find("CABR")).text).split()
+        self.NumberOfCabrReactions = int(Cabr[0])
+        listIndicesOfCabrReactions = Cabr[1:]
+        self.IndicesOfCabrReactions = [int(i) for i in listIndicesOfCabrReactions] 
+        
+    
+    def returnNameFromIndex(self, reactionIndex: int):
+        # reactionIndex 0-based
+        if(reactionIndex + 1 <= self.nr):
+
+            name = "R" + str(reactionIndex + 1) + ": " + self.reaction_strings_[reactionIndex]
+            return name
+        else:
+
+            local_index = reactionIndex + 1 - self.nr
+            if(local_index <= self.NumberOfFallOffReactions):
+
+                global_index = self.IndicesOfFallOffReactions[local_index-1]
+                name = "R" + str(global_index) + "(inf): " + self.reaction_strings_[global_index-1]
+                return name
+            else:
+
+                global_index = self.IndicesOfCabrReactions[local_index- self.nr - 1]
+                name = "R" + str(global_index) + "(inf): " + self.reaction_strings_[global_index-1]
+                return name
