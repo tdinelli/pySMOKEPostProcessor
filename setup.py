4539fdbdb069866b0556fb67a0838df6b066c68f
timoteo dinelli <timoteo.dinelli@polimi.it>
2023-03-14 19:01:09 +0100

    Minors

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 import setuptools
 
 setuptools.setup(name="pySMOKEPostProcessor",
-  version= '0.2.0',
-  description='Python Binder of the OpenSMOKE post processor',
-  author='Timoteo Dinelli',
-  author_email= 'timoteo.dinelli@polimi.it',
-  packages=['pySMOKEPostProcessor',
-	'pySMOKEPostProcessor.amech_utils',
-	'pySMOKEPostProcessor.maps',
-	'pySMOKEPostProcessor.rxnclass',
-	'pySMOKEPostProcessor.plots',
+	version= '0.2.0',
+	description='Python Binder of the OpenSMOKE post processor',
+	author='Timoteo Dinelli',
+	author_email= 'timoteo.dinelli@polimi.it',
+	packages=['pySMOKEPostProcessor',
+		'pySMOKEPostProcessor.amech_utils',
+		'pySMOKEPostProcessor.maps',
+		'pySMOKEPostProcessor.rxnclass',
+		'pySMOKEPostProcessor.plots',
 	],
-  package_dir={'pySMOKEPostProcessor':'pySMOKEPostProcessor',},
-  install_requires=['numpy', 'pandas', 'matplotlib', 'networkx', 'jupyter', 'pydot', 'graphviz', 'scipy'],
-  keywords = ['Sensitivity Analysis', 'Elemental Flux Analysis', 'Rate Of Production', 'kinetics', 'opensmokepp'],
-  include_package_data=True,
+	package_dir={'pySMOKEPostProcessor':'pySMOKEPostProcessor',},
+	install_requires=['numpy', 'pandas', 'matplotlib', 'networkx', 'jupyter', 'pydot', 'graphviz', 'scipy'],
+	keywords = ['Sensitivity Analysis', 'Elemental Flux Analysis', 'Rate Of Production', 'kinetics', 'opensmokepp'],
+	include_package_data=True,
 )
 
 '''
