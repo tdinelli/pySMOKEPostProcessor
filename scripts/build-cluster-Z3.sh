7fb380a1f6e15e36d4b38851b6b85924e5a570b8
Titodinelli <timoteo.dinelli@polimi.it>
2023-03-16 09:43:46 +0100

    Add bash configuration files for linux and Z3 cluster

---
+++ scripts/build-cluster-Z3.sh
@@ -0,0 +1,15 @@
+export Eigen3_ROOT="/software/chimica2/libraries/eigen/eigen-3.4.0"
+export Boost_ROOT="/software/chimica2/libraries/boost/boost-1.78.0-gcc-9.2.0"
+export OpenSMOKEpp_ROOT="/home/chimica2/tdinelli/GitHub/OpenSMOKEpp"
+
+cd ..
+rm -rf build
+mkdir build
+cd build
+cmake \
+    -DCMAKE_C_COMPILER=/software/chimica2/tools/gcc/gcc-9.2.0/bin/gcc \
+    -DCMAKE_CXX_COMPILER=/software/chimica2/tools/gcc/gcc-9.2.0/bin/gcc \
+    -DARCH=x86-64 \
+    ..
+
+make
