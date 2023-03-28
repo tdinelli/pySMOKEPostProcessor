05c2e0d9a0d2fcaba27abaf2cb2f07e957611b3d
Titodinelli <timoteo.dinelli@polimi.it>
2023-03-28 23:49:02 +0200

    Renaming

--- scripts/build-cluster-Z3.sh
+++ scripts/build-cluster-Z3.sh
@@ -1,8 +1,8 @@
-export Eigen3_ROOT="/software/chimica2/libraries/eigen/eigen-3.4.0"
-export Boost_ROOT="/software/chimica2/libraries/boost/boost-1.78.0-gcc-9.2.0"
+#export Eigen3_ROOT="/software/chimica2/libraries/eigen/eigen-3.4.0"
+#export Boost_ROOT="/software/chimica2/libraries/boost/boost-1.78.0-gcc-9.2.0"
+#export CC=/software/chimica2/tools/gcc/gcc-9.2.0/bin/gcc
+#export CXX=/software/chimica2/tools/gcc/gcc-9.2.0/bin/g++
 export OpenSMOKEpp_ROOT="/home/chimica2/tdinelli/GitHub/OpenSMOKEpp"
-export CC=/software/chimica2/tools/gcc/gcc-9.2.0/bin/gcc
-export CXX=/software/chimica2/tools/gcc/gcc-9.2.0/bin/g++
 export ARCH=x86-64
 
 # --- Do not modify below this
@@ -16,3 +16,6 @@ cmake \
 
 make
 make install
+cd ..
+python setup.py bdist_wheel
+pip install dist/pySMOKEPostProcessor-0.3.0-py3-none-any.whl
