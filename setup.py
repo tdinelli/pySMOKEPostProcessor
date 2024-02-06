765fe7c77b6b4e572debc1004295b23423b291b7
timoteo dinelli <timoteo.dinelli@polimi.it>
2024-02-05 17:10:57 -0800

    Add new consistent python installation

--- setup.py
+++ setup.py
@@ -1,49 +1,153 @@
-# based on https://github.com/pybind/scikit_build_example/blob/master/setup.py
-
+# For the details see: https://github.com/pybind/cmake_example/blob/master/setup.py
 import os
+import re
+import subprocess
 import sys
+from pathlib import Path
+from setuptools import Extension, setup, find_packages
+from setuptools.command.build_ext import build_ext
 
-from setuptools import find_namespace_packages
-try:
-    import tomllib
-except ModuleNotFoundError:
-    import tomli as tomllib
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-sys.path.insert(0, os.path.dirname(__file__))
-try:
-    from skbuild import setup
-except ImportError:
-    print("Please update pip, you need pip 10 or greater", file=sys.stderr)
-    raise
-
-with open('pyproject.toml', 'rb') as f:
-    pyproject = tomllib.load(f)
+# Convert distutils Windows platform specifiers to CMake -A arguments
+PLAT_TO_CMAKE = {
+    "win32": "Win32",
+    "win-amd64": "x64",
+    "win-arm32": "ARM",
+    "win-arm64": "ARM64",
+}
 
 
-def load_long_description():
-    with open(pyproject["project"]["readme"], mode='r', encoding="utf-8") as f:
-        return f.read()
+# A CMakeExtension needs a sourcedir instead of a file list.
+# The name must be the _single_ output extension from the CMake build.
+# If you need multiple extensions, see scikit-build.
+class CMakeExtension(Extension):
+    def __init__(self, name: str, sourcedir: str = "") -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = os.fspath(Path(sourcedir).resolve())
 
 
-# cmake_args = [
-#     f"-DDEEPTIME_VERSION={versioneer.get_version().split('+')[0]}",
-#     f"-DDEEPTIME_VERSION_INFO={versioneer.get_version()}"
-# ]
+class CMakeBuild(build_ext):
+    def build_extension(self, ext: CMakeExtension) -> None:
+        # Must be in this form due to bug in .resolve() only fixed in Python 3.10+
+        ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)
+        extdir = ext_fullpath.parent.resolve()
 
-excludes = ("Test", "examples", "external", "cmake", "cxx_wrapper", "devtools",
-            "build")
+        # Using this requires trailing slash for auto-detection & inclusion of
+        # auxiliary "native" libs
 
-metadata = \
-    dict(
-        long_description=load_long_description(),
-        long_description_content_type='text/markdown',
-        zip_safe=False,
-        packages=find_namespace_packages(where=".", exclude=excludes),
-        package_dir={"pySMOKEPostProcessor": "pySMOKEPostProcessor"},
-        cmake_install_dir="deeptime/",
-        include_package_data=True,
-        ext_modules=[],
-    )
+        debug = int(os.environ.get("DEBUG", 0)
+                    ) if self.debug is None else self.debug
+        # TODO
+        cfg = "Debug" if debug else "Release"
+        cfg = "Release"
 
-if __name__ == '__main__':
-    setup(**metadata)
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
+        # from Python.
+        # print("CIAO TITOTTTTTT", f"-D CMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}{"pySMOKEPostProcessor"}")
+        cmake_args = [
+            f"-D CMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}{"pySMOKEPostProcessor"}",
+            f"-D CMAKE_CXX_COMPILER:PATH={os.environ["CXX"]}",
+            f"-D Boost_ROOT:PATH={os.environ["Boost_ROOT"]}",
+            f"-D Eigen3_DIR:PATH={os.environ["Eigen3_ROOT"]}",
+        ]
+        build_args = []
+        # Adding CMake arguments set as environment variable
+        # (needed e.g. to build for ARM OSx on conda-forge)
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [
+                item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+
+        # In this example, we pass in the version to C++. You might not need to.
+        cmake_args += [f"-DEXAMPLE_VERSION_INFO={
+            self.distribution.get_version()}"]
+
+        if self.compiler.compiler_type != "msvc":
+            # Using Ninja-build since it a) is available as a wheel and b)
+            # multithreads automatically. MSVC would require all variables be
+            # exported for Ninja to pick it up, which is a little tricky to do.
+            # Users can override the generator with CMAKE_GENERATOR in CMake
+            # 3.15+.
+            if not cmake_generator or cmake_generator == "Ninja":
+                try:
+                    import ninja
+
+                    ninja_executable_path = Path(ninja.BIN_DIR) / "ninja"
+                    cmake_args += [
+                        "-GNinja",
+                        f"-DCMAKE_MAKE_PROGRAM:FILEPATH={
+                            ninja_executable_path}",
+                    ]
+                except ImportError:
+                    pass
+
+        else:
+            # Single config generators are handled "normally"
+            single_config = any(
+                x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                cmake_args += [
+                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
+                ]
+                build_args += ["--config", cfg]
+
+        if sys.platform.startswith("darwin"):
+            # Cross-compile support for macOS - respect ARCHFLAGS if set
+            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+            if archs:
+                cmake_args += [
+                    "-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
+
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
+            if hasattr(self, "parallel") and self.parallel:
+                # CMake 3.12+ only.
+                build_args += [f"-j{self.parallel}"]
+
+        build_temp = Path(self.build_temp) / ext.name
+        if not build_temp.exists():
+            build_temp.mkdir(parents=True)
+
+        subprocess.run(
+            ["cmake", ext.sourcedir, *cmake_args], cwd=build_temp, check=True
+        )
+        subprocess.run(
+            ["cmake", "--build", ".", *build_args], cwd=build_temp, check=True
+        )
+
+
+# The information here can also be placed in setup.cfg - better separation of
+# logic and declaration, and simpler if you include description/version in a file.
+# packages=find_packages(exclude=["__pycache__"]),
+excludes = ["tests", "tests.*", "examples", "examples.*", "Test/",
+            "docs", "docs.*", "devtools", "devtools.*", "__pycache__/"]
+setup(
+    name="pySMOKEPostProcessor",
+    version="0.0.1",
+    author="Timoteo Dinelli",
+    packages=find_packages(where=".", exclude=excludes),
+    include_package_data=True,
+    ext_modules=[CMakeExtension("pySMOKEPostProcessor")],
+    cmdclass={"build_ext": CMakeBuild},
+    zip_safe=False,
+)
