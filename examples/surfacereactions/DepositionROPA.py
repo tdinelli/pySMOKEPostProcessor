c45e17c113e0ec5f71a88631bcfab160bdfe08c9
lgiardini <lory.giardi@gmail.com>
2026-04-16 19:38:23 +0200

    Included Soot in deposition_plot (time integral view of soot production) and updated the other related files accordingly

--- examples/surfacereactions/DepositionROPA.py
+++ examples/surfacereactions/DepositionROPA.py
@@ -1,20 +1,33 @@
 import os
 import matplotlib.pyplot as plt
-from pySMOKEPostProcessor.plotting_utilities.bar_plot import plot_bars
-from pySMOKEPostProcessor import plot_heatmap
-from pySMOKEPostProcessor import script_utils
-from pySMOKEPostProcessor.surfacereactions_utilities.deposition_plot import CumulativeDeposition
+# from pySMOKEPostProcessor.plotting_utilities.bar_plot import plot_bars
+# from pySMOKEPostProcessor import plot_heatmap
+# from pySMOKEPostProcessor import script_utils
+from pySMOKEPostProcessor.surfacereactions_utilities.deposition_plot import CumulativeDeposition,CumulativeSootProduction
 
 kineticFolder = os.path.join("..","data","Surface_Data","kinetics")
 # kineticFile = os.path.join(kineticFolder,"kinetics.surface.xml")
 resultsFolder = os.path.join("..","data","Surface_Data","Output")
 
-class_groups_file = os.path.join('..', 'data', 'Surface_Data', 'surf_rxn_class.txt')
+surface_classes = os.path.join('..', 'data', 'Surface_Data', 'surf_rxn_class.txt')
+class_groups_file = os.path.join('..', 'data', 'ReactionClasses', 'rxn_class_groups.txt')
 sortlists = [['reactiontype']]  # Ask Luna why this is a list of lists  # classtype, speciestype, subclass, bimoltype, reactiontype
 
-obj = CumulativeDeposition(kineticFolder=kineticFolder, outputFolder=resultsFolder, class_group_file=class_groups_file,allCarbon=False)
+lump_steps = 4  # To make it faster (less resolution)
 
-obj.setClassification(sort_type=sortlists)
-obj.plotCumulativeDeposition(lump_steps=1,units='thickness')
+Bulk = CumulativeDeposition(    kineticFolder=kineticFolder, outputFolder=resultsFolder, class_group_file=surface_classes,  sort_type=sortlists, area=200./1E4, allCarbon=True)
+Soot = CumulativeSootProduction(kineticFolder=kineticFolder, outputFolder=resultsFolder, class_group_file=class_groups_file,sort_type=sortlists,volume=100./1E4)
 
+fig,ax = plt.subplots(2,1,figsize=(10.5,12.), sharex=True)
+
+fig,ax[0] = Bulk.plotCumulativeDeposition(lump_steps=lump_steps,units='mass',fig=fig,ax=ax[0])
+fig,ax[1] = Soot.plotSootProduction(      lump_steps=lump_steps,units='mass',fig=fig,ax=ax[1])
+
+ymax = max(ax[0].get_ylim()[1], ax[1].get_ylim()[1])
+ax[0].set_ylim(top=ymax*1.1)
+ax[1].invert_yaxis()
+ax[1].set_ylim(bottom=ymax*1.1)
+
+fig.tight_layout()
+#fig.savefig("Deposition_Soot.png", dpi=300)
 plt.show()
